663bc5db4d98c583d5a4025718f02a3a361a23c9
Navneet Verma <47264956+papivot@users.noreply.github.com>
2025-08-28 19:25:35 -0700

    Chiseled

--- docker/exec_on_node.sh
+++ docker/exec_on_node.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+set -e
 echo "Starting execution of install script"
 echo "Copying required binaries and files to the host filesystem"
 cp /host-install-files/* /host
 
-/usr/bin/nsenter -a -t 1 chmod +x /tmp/install/install.sh
-/usr/bin/nsenter -a -t 1 chmod +x /tmp/install/wait.sh
+/usr/bin/nsenter -a -t 1 chmod +x /var/tmp/install/install.sh
+/usr/bin/nsenter -a -t 1 chmod +x /var/tmp/install/wait.sh
 
 echo "Executing wait script for the node to become ready for script execution"
-/usr/bin/nsenter -a -t 1 /tmp/install/wait.sh
+/usr/bin/nsenter -a -t 1 /var/tmp/install/wait.sh
 
 echo "Executing script"
-/usr/bin/nsenter -a -t 1 /tmp/install/install.sh
+/usr/bin/nsenter -a -t 1 /var/tmp/install/install.sh
 
 echo "Sleeping..."
 while true;
